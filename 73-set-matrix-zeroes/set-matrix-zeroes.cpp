9ccdb1122de9d29d2ddd34948cd17429f7b73745
mdirfan4514 <irfan965168@gmail.com>
2026-06-30 13:52:06 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (93.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,23 +1,56 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       int m = matrix.size();
-       int n = matrix[0].size();
-       
-       vector<vector <int>> temp = matrix;
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(matrix[i][j]==0){
-              for(int k=0;k<n;k++){
-                 temp[i][k] = 0;
-                  }
-                    for(int k=0;k<m;k++){
-                      temp[k][j] = 0;
-              }
+
+        int m = matrix.size();
+         int n = matrix[0].size();
+
+         bool firstRowImpacted = false;
+         bool firstColImpacted = false;
+
+         // check first row is impacted or not
+         for(int j = 0;j<n; j++){
+            if(matrix[0][j] == 0){
+                firstRowImpacted = true;
             }
-          }
-       }
-       matrix = temp;
+         }
+
+         // check first col is impacted or not
+         for(int i = 0; i<m; i++){
+            if(matrix[i][0] == 0){
+                firstColImpacted = true;
+            }
+         }
+
+         // now rows and col
+         for(int i =1; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+         }
+
+         for(int i =1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+         }
+         
+         if(firstRowImpacted){
+         for(int j=0; j<n; j++){
+             matrix[0][j] = 0;
+         }
+      }
+
+         if(firstColImpacted){
+         for(int i=0; i<m; i++){
+         matrix[i][0] = 0;
+         }
+      }
         
     }
 };
