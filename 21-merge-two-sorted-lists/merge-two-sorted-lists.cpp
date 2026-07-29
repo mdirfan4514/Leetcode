1d8ce186f6c2cf67f2e8006ed1d8f0f8a0166042
mdirfan4514 <irfan965168@gmail.com>
2026-07-29 13:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (28.31%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
-        ListNode* temp1 = list1;
-        ListNode* temp2 = list2;
-        ListNode* c   = new ListNode(10);
-        ListNode* temp3 = c;
-        while(temp1!=NULL && temp2!=NULL){
-            if(temp1->val < temp2->val){
-                ListNode* t = new ListNode(temp1->val);
-                temp3->next = t;
-                temp3 = t;
-                temp1 = temp1->next;
-            }
-            else{
-                ListNode* t = new ListNode(temp2->val);
-                temp3->next = t;
-                temp3 = t;
-                temp2 = temp2->next;
-            }
-        }
-        if(temp1==NULL){
-             temp3->next = temp2;
+       ListNode* a = new ListNode(0);
+       ListNode* temp = a;
+       while(list1!=NULL && list2!=NULL){
+        if(list1->val < list2->val){
+            temp->next = list1;
+            list1 = list1->next;
+            temp = temp->next;
         }
         else{
-            temp3->next = temp1;
+            temp->next = list2;
+            list2 = list2->next;
+            temp = temp->next;
         }
-        return c->next;
+       }
+       if(list1!=NULL){
+        temp->next = list1;
+       }
+       else{
+        temp->next = list2;
+       }
+       return a->next;
     }
 };
