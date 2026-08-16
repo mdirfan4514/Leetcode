c5caa82a7d26a4b427d8cca3da5a5771ffb86298
mdirfan4514 <irfan965168@gmail.com>
2026-08-16 14:03:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (53.96%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,32 @@
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int n = q.size();
+        for(int i = 0; i<n-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+       int ans = q.front();
+       q.pop();
+       return ans;
+    }
+    
+    int top() {
+        int x = q.front();
+        return x;
+    }
+    
+    bool empty() {
+        if(q.size()==0) return true;
+        else return false;
+    }
+};
