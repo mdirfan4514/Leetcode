897bed56811eee0e67b9a88ce242db17351a5a2e
mdirfan4514 <irfan965168@gmail.com>
2026-08-21 11:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (61.29%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() > nums2.size()){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
         int m = nums1.size();
         int n = nums2.size();
-        vector<int> ans;
-        int i = 0;
-        int j = 0;
-        while(i<m && j<n){
-            if(nums1[i] <= nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-            }
-            else{
-                ans.push_back(nums2[j]);
-                j++;
-            }
-        }
-
-        while(i<m){
-            ans.push_back(nums1[i]);
-            i++;
-        }
-
-        while(j<n){
-            ans.push_back(nums2[j]);
-            j++;
-        }
         
-        int size = ans.size();
-        if(size%2 != 0){
-            return ans[size/2];
-        }
-        else {
-            return (ans[size/2-1] + ans[size/2])/2.0;
-        }
+        int l = 0;
+        int h = m;
 
+        while(l <= h){
+            int Px = l + (h-l)/2;
+            int Py = (m+n+1)/2 - Px;
+
+            int x1 = (Px == 0) ? INT_MIN : nums1[Px-1];
+            int x2 = (Py == 0) ? INT_MIN : nums2[Py-1];
+            int x3 = (Px == m) ? INT_MAX : nums1[Px];
+            int x4 = (Py == n) ? INT_MAX : nums2[Py];
+
+            if(x1 <= x4 && x2 <= x3){
+                if((m+n)%2 == 1){
+                    return max(x1, x2);
+                }
+                else{
+                return (max(x1, x2) + min(x3,x4))/2.0;
+              }
+            }
+            if(x1 > x4) h = Px-1;
+            else l = Px + 1;              
+        }
+        return -1; 
     }
 };
