091ce5b659688066efadd9a02adfbde2b0ac6fc4
mdirfan4514 <irfan965168@gmail.com>
2026-08-24 14:21:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (26.28%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -5,8 +5,8 @@ public:
         TreeNode* temp = root->left;
         root->left = root->right;
         root->right = temp;
-        invertTree(root->left);
         invertTree(root->right);
+        invertTree(root->left);
         return root;
     }
 };
