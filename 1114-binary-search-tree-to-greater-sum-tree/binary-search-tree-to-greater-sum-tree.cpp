931c33cc05b8dff82d3910c80a76490b2cac7601
mdirfan4514 <irfan965168@gmail.com>
2026-09-04 08:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (78.64%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int sum = 0;
+    TreeNode* bstToGst(TreeNode* root) {
+        if(root==NULL) return NULL;
+        bstToGst(root->right);
+        root->val = root->val + sum;
+        sum = root->val;
+        bstToGst(root->left);
+        return root;
+    }
+};
