51a53d97109fb5b4ed02442f04d160264083d488
Mohammad Irfan <irfan965168@gmail.com>
2026-09-11 09:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (24.08%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<TreeNode*> &ans){
-        if(root==NULL) return;
-        ans.push_back(root);
-        preorder(root->left, ans);
-        preorder(root->right, ans);
-    }
     void flatten(TreeNode* root) {
-        vector<TreeNode*> ans;
-        preorder(root, ans);
-        int n = ans.size();
-        for(int i=0; i<n-1; i++){
-            ans[i]->right = ans[i+1];
-            ans[i]->left = NULL;
-        }
-        if(n > 0){
-            ans[n-1]->left = NULL;
-        }
+        if(root==NULL) return;
+        TreeNode* l = root->left;
+        TreeNode* r = root->right;
+        root->left = NULL;
+        root->right = NULL;
+        flatten(l);
+        flatten(r);
+        root->right = l;
+        TreeNode* temp = root;
+        while(temp->right != NULL) temp = temp->right;
+        temp->right = r;
         return;
     }
 };
