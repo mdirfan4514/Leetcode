da484da302b330ce2ac17870ffe962aeb473e0ec
Mohammad Irfan <irfan965168@gmail.com>
2026-09-15 12:54:46 +0530

    Time: 110 ms (12.45%) | Memory: 83.9 MB (17.51%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    typedef pair<int, vector<int>> piv;
+    vector<vector<int>> kClosest(vector<vector<int>>& arr, int k) {
+        priority_queue<piv> pq;
+        for(vector<int> v : arr){
+            int x = v[0], y = v[1];
+            int dist = x*x + y*y;
+            pq.push({dist,v});
+            if(pq.size()>k) pq.pop();
+        }
+        vector<vector<int>> ans;
+        while(pq.size()>0){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
