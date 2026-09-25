81302bbcb342f8c3f7f5bd165f81fb65080c9452
Mohammad Irfan <irfan965168@gmail.com>
2026-09-25 19:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (12.43%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        int n = stones.size();
         priority_queue<int> pq;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<stones.size(); i++){
             pq.push(stones[i]);
         }
         while(pq.size() > 1){
